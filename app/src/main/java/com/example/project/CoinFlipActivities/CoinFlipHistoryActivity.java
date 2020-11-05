0c3e4eab7fc367d275e195ee2a71c1a0803a0db9
Kent Nex <knex@sfu.ca>
2020-11-05 12:20:30 -0800

    Add: View single child history.
    Added the ability to view a single child's history.

--- app/src/main/java/com/example/project/CoinFlipActivities/CoinFlipHistoryActivity.java
+++ app/src/main/java/com/example/project/CoinFlipActivities/CoinFlipHistoryActivity.java
@@ -19,12 +19,18 @@ import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import com.example.project.R;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CoinFlipHistoryActivity extends AppCompatActivity {
 
@@ -36,10 +42,17 @@ public class CoinFlipHistoryActivity extends AppCompatActivity {
     private ChildManager childManager;
     private CoinFlipHistoryManager flipManager;
 
+    private List<CoinFlipHistoryMember> chosenChildHistory = new ArrayList<>();
+
     public static Intent makeLaunchIntent(Context context, int index) {
         Intent intent = new Intent(context, CoinFlipHistoryActivity.class);
         intent.putExtra(EXTRA_INDEX, index);
         return intent;
+    }
+
+    private void extractDataFromIntent(){
+        Intent intent = getIntent();
+        childIndex = intent.getIntExtra(EXTRA_INDEX, -1);
     }
 
     @Override
@@ -49,32 +62,77 @@ public class CoinFlipHistoryActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        extractDataFromIntent();
         childManager = ChildManager.getInstance();
         flipManager = CoinFlipHistoryManager.getInstance();
+        chosenChildHistory = getHistoryOfOneChildFromIndex(childIndex);
+        addOnClickToggle();
+        setButtonToChildName();
 
         // Enable "up" on toolbar
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
 
         Log.println(Log.INFO, CHILDMANAGER_TAG, childManager.getLength() + "");
-        populateListView();
+        populateListView(flipManager.getFlipList());
+    }
+
+    private void addOnClickToggle(){
+        ToggleButton toggle = (ToggleButton) findViewById(R.id.coin_flip_history_toggle);
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    populateListView(chosenChildHistory);
+                } else {
+                    populateListView(flipManager.getFlipList());
+                }
+            }
+        });
+    }
+
+    private void setButtonToChildName(){
+        String text = getString(R.string.coin_flip_history_single);
+        String childName = ((childIndex != -1) ?  childManager.getChildName(childIndex) : "Child Not Found.");
+        ToggleButton toggleButton = (ToggleButton) findViewById(R.id.coin_flip_history_toggle);
+
+        toggleButton.setTextOff(String.format(text, childName));
+        toggleButton.setText(String.format(text, childName));
+
+    }
+
+    private List<CoinFlipHistoryMember> getHistoryOfOneChildFromIndex(int index){
+
+        List<CoinFlipHistoryMember> childList = new ArrayList<>();
+        int childID = childManager.getChildID(index);
+
+        for(CoinFlipHistoryMember flip : flipManager.getFlipList()){
+
+            if(flip.getChildID() == childID){
+                childList.add(flip);
+            }
+        }
+
+        return childList;
     }
 
 
-    // Populates the List View with lenses.
-    private void populateListView(){
+    private void populateListView(List<CoinFlipHistoryMember> coinFlipList){
 
-        ListAdapter adapter = new CoinFlipHistoryActivity.MyListAdapter();
+        ListAdapter adapter = new CoinFlipHistoryActivity.MyListAdapter(coinFlipList);
 
-        // Configure
         ListView list = (ListView) findViewById(R.id.coin_flip_history_list);
         list.setAdapter(adapter);
+
     }
 
     // Adds an adapter to the list view. Allows for on images to be added.
     private class MyListAdapter extends ArrayAdapter<CoinFlipHistoryMember> {
-        public MyListAdapter(){
-            super(CoinFlipHistoryActivity.this, R.layout.history_list, flipManager.getFlipList());
+
+        List<CoinFlipHistoryMember> flipList;
+
+        public MyListAdapter(List<CoinFlipHistoryMember> coinFlipList){
+            super(CoinFlipHistoryActivity.this, R.layout.history_list, coinFlipList);
+            flipList = coinFlipList;
         }
 
         @Override
@@ -83,7 +141,7 @@ public class CoinFlipHistoryActivity extends AppCompatActivity {
             int childIndex = -1;
 
             try {
-                childIndex = childManager.findChildIndexById(flipManager.getFlipChildID(position));
+                childIndex = childManager.findChildIndexById(flipList.get(position).getChildID());
             } catch (Exception e){
                 Log.println(Log.ERROR, CHILDMANAGER_TAG, "Failed to load: " + e.getMessage());
             }
@@ -99,8 +157,8 @@ public class CoinFlipHistoryActivity extends AppCompatActivity {
             ImageView winLoseIcon = (ImageView) itemView.findViewById(R.id.coin_flip_history_win_lose_image);
             ImageView headsTailsIcon = (ImageView) itemView.findViewById(R.id.coin_flip_history_coin_chosen);
 
-            winLoseIcon.setImageResource(flipManager.getFlipWinLoseIcon(position));
-            headsTailsIcon.setImageResource(flipManager.getFlipHeadsTailsIcon(position));
+            winLoseIcon.setImageResource(flipList.get(position).getWinLoseIcon());
+            headsTailsIcon.setImageResource(flipList.get(position).getHeadsTailsIcon());
 
             if(childIndex == -1) {
                 childAvatar.setImageResource(R.drawable.default_avator);
