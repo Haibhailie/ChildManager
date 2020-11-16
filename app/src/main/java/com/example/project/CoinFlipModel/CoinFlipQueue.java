3fb0121bb53b7190bda8d761c1b04dba217e311d
Kent Nex <knex@sfu.ca>
2020-11-16 12:03:25 -0800

    Add: Coin Flip Queue
    Added a coin flip queue to record the order the children are in
    for the coin flip.

---
+++ app/src/main/java/com/example/project/CoinFlipModel/CoinFlipQueue.java
@@ -0,0 +1,60 @@
+package com.example.project.CoinFlipModel;
+
+import com.example.project.ChildModel.Child;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CoinFlipQueue {
+
+    private static CoinFlipQueue instance;
+    private List<Integer> childIdQueue = new ArrayList<>();
+
+    public static CoinFlipQueue getInstance(){
+        if (instance == null){
+            instance = new CoinFlipQueue();
+        }
+        return instance;
+    }
+
+    public void addId(int id){
+        childIdQueue.add(id);
+    }
+
+    public void removeId(int id){
+        childIdQueue.remove(id);
+    }
+
+    public void putToBack(int id){
+        childIdQueue.remove(id);
+        childIdQueue.add(id);
+    }
+
+    public void setQueue(List<Integer> savedQueue){
+        childIdQueue = savedQueue;
+    }
+
+    public void removeMissingIds(List<Integer> childIdList){
+
+        List<Integer> newChildIdQueue = new ArrayList<>();
+
+        for(int id : childIdQueue){
+            if(childIdList.contains(id)){
+                newChildIdQueue.add(id);
+            }
+        }
+
+        childIdQueue = newChildIdQueue;
+    }
+
+    public void addMissingNewIds(List<Integer> childIdList){
+        for(int id : childIdList){
+            if(!childIdQueue.contains(id)){
+                childIdQueue.add(id);
+            }
+        }
+    }
+
+
+
+}
