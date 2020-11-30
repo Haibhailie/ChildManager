25c0b195d885134b7348ef8979c99d9c762201d2
Kent Nex <knex@sfu.ca>
2020-11-30 12:54:41 -0800

    Fix: Model to match class
    Fixed the model to match how it was done in class.

--- app/src/main/java/com/example/project/takebreathactivities/TakeBreathActivity.java
+++ app/src/main/java/com/example/project/takebreathactivities/TakeBreathActivity.java
@@ -4,21 +4,23 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.project.MainActivity;
-import com.example.project.takebreathmodel.BreathState;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.os.Handler;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.project.R;
 
 public class TakeBreathActivity extends AppCompatActivity {
 
-    private BreathState breath;
+    private static int THREE_SECONDS = 3000;
+    private static int SEVEN_SECONDS = 7000;
 
     public static Intent makeLaunchIntent(MainActivity context) {
         Intent intent = new Intent(context, TakeBreathActivity.class);
@@ -31,32 +33,146 @@ public class TakeBreathActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        breath = new BreathState();
+        setState(waitingInhaleState);
         setupBeginButton();
     }
 
     private void setupBeginButton() {
         Button breathButton = (Button) findViewById(R.id.take_breath_breath_button);
+
         breathButton.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                if(event.getAction() == MotionEvent.ACTION_DOWN){
-                    buttonDown();
+                if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                    currentState.handleClickOn();
                 } else if (event.getAction() == MotionEvent.ACTION_UP){
-                    buttonUp();
+                    currentState.handleClickOff();
                 }
                 return true;
             }
         });
     }
 
-    private void buttonDown(){
-        breath.changeState();
-        Log.println(Log.INFO, "BREATH_BUTTON", "DOWN");
+    private void setButtonText(String text){
+        Button breathButton = (Button) findViewById(R.id.take_breath_breath_button);
+        breathButton.setText(text);
     }
 
-    private void buttonUp(){
-        breath.changeState();
-        Log.println(Log.INFO, "BREATH_BUTTON", "UP");
+    private void setHelpText(String text){
+        TextView helpText = (TextView) findViewById(R.id.take_breath_help_text);
+        helpText.setText(text);
     }
+
+    private final State waitingInhaleState = new WaitingInhaleState();
+    private final State inhaleState = new InhaleState();
+    private final State exhaleState = new ExhaleState();
+
+    private State currentState = new IdleState();
+
+    private void setState(State newState){
+        currentState.handleExit();
+        currentState = newState;
+        currentState.handleEnter();
+    }
+
+
+    public abstract class State{
+        void handleEnter() {}
+        void handleExit(){}
+        void handleClickOff() {}
+        void handleClickOn() {}
+    }
+
+    // INHALE STATE
+    private class InhaleState extends State {
+        Handler timerHandler = new Handler();
+        Runnable timerRunnable = () -> setState(exhaleState);
+
+        @Override
+        void handleEnter() {
+            setButtonText("In");
+            setHelpText("Hold button and breath in");
+
+            Log.println(Log.INFO, "STATE", "Inhale Enter");
+        }
+
+        @Override
+        void handleExit() {
+            timerHandler.removeCallbacks(timerRunnable);
+            Log.println(Log.INFO, "STATE", "Inhale Exit");
+        }
+
+        @Override
+        void handleClickOff() {
+            timerHandler.removeCallbacks(timerRunnable);
+            Log.println(Log.INFO, "STATE", "Inhale Click Off");
+        }
+
+        @Override
+        void handleClickOn() {
+            timerHandler.postDelayed(timerRunnable, THREE_SECONDS);
+            Log.println(Log.INFO, "STATE", "Inhale Click On");
+        }
+    }
+
+    // EXHALE STATE
+    private class ExhaleState extends State {
+        Handler timerHandler = new Handler();
+        Runnable timerRunnable = () -> setState(inhaleState);
+        Runnable heldTooLongRunnable = () -> heldTooLong();
+
+        @Override
+        void handleEnter() {
+            setButtonText("Out");
+            setHelpText("Let go off button and breath out");
+            timerHandler.postDelayed(heldTooLongRunnable, SEVEN_SECONDS);
+
+            Log.println(Log.INFO, "STATE", "Exhale Enter");
+        }
+
+        @Override
+        void handleExit() {
+            timerHandler.removeCallbacks(timerRunnable);
+            Log.println(Log.INFO, "STATE", "Exhale Exit");
+        }
+
+        @Override
+        void handleClickOff() {
+            timerHandler.removeCallbacks(heldTooLongRunnable);
+            timerHandler.postDelayed(timerRunnable, THREE_SECONDS);
+            Log.println(Log.INFO, "STATE", "Exhale Click Off");
+        }
+
+        @Override
+        void handleClickOn() {
+            timerHandler.removeCallbacks(timerRunnable);
+            Log.println(Log.INFO, "STATE", "Exhale Click On");
+        }
+
+        void heldTooLong(){
+            setHelpText("LET GO");
+            Log.println(Log.INFO, "STATE", "Inhale Held too long");
+        }
+    }
+
+    // WAITING INHALE STATE
+    private class WaitingInhaleState extends State {
+        @Override
+        void handleClickOn() {
+            setState(inhaleState);
+        }
+    }
+
+    // NULL STATE
+    private class IdleState extends State {
+    }
+
+
+
+
+
+
+
+
+
 }
